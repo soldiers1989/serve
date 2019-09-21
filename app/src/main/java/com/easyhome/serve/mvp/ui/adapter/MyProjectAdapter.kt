e2bfba3dca307ebf4aaf1cfc8cc266df6a630048
lpl0129 <lpl0129@126.com>
2019-09-21 10:02:07 +0800

    优化

--- app/src/main/java/com/easyhome/serve/mvp/ui/adapter/MyProjectAdapter.kt
+++ app/src/main/java/com/easyhome/serve/mvp/ui/adapter/MyProjectAdapter.kt
@@ -1,5 +1,6 @@
 package com.easyhome.serve.mvp.ui.adapter
 
+import android.support.v7.widget.RecyclerView
 import android.text.Html
 import android.widget.ImageView
 import com.chad.library.adapter.base.BaseQuickAdapter
@@ -11,9 +12,9 @@ class MyProjectAdapter(data: List<String>) :
         (R.layout.my_project_layout, data) {
 
     override fun convert(helper: BaseViewHolder, item: String) {
-
         helper.addOnClickListener(R.id.nameTV)
-        helper.setText(R.id.locationTV,Html.fromHtml("客户编号：<font color='#777777'>1234</font>"))
+        helper.setText(R.id.locationTV, Html.fromHtml("客户编号：<font color='#777777'>1234</font>"))
+        helper.getView<RecyclerView>(R.id.tagRV).adapter = Tag3Adapter(arrayListOf("", "", "","",""))
     }
 
 
